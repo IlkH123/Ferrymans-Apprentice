cb933fedd2db19d73f99511e3a83c3b62650c9b7
trollusTuberius <ri.huhta@gmail.com>
2023-03-29 20:40:32 +0300

    Various changes
    +Persistent player model
    +improvements to scene management
    -removed moving scripts from individual work folders because they were causing conflicts
    +copied assets into the root assets folder
    +preliminary animation integration to the protoscenes, severely unfinished
    etc.

--- Assets/Scripts/Ilkka/CameraFocus.cs
+++ Assets/Scripts/Ilkka/CameraFocus.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraFocus : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class CameraFocus : MonoBehaviour
     Vector3 cameraRestPoint;
     float defaultZ;
     public bool groundCheck;
+    int currentSceneIndex;
 
     // Margin points in viewport coordinates,
     // values range from 0 to 1 where 0,0 is bottom left
@@ -24,20 +26,76 @@ public class CameraFocus : MonoBehaviour
         edgeVectorBottomRight;
 
     [SerializeField]
-    float marginLeft, marginRight, marginUp, marginDown;
+    float marginLeft, marginRight, marginUp, marginDown, marginBottomFloor;
     [SerializeField]
     float newX, newY, restPointX, restPointY;
 
+    //Next two categories are a delegate method for the SceneManager namespace,
+    //Not exactly sure how it works, don't mess with it if you don't have to
+    //Point is to call the OnSceneLoaded() when the scene changes, persistent model
+    //means awake will not be called on scene change, hence we need this one
 
-    // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
-        camera = Camera.main;
-        defaultZ = camera.transform.position.z;
-        cameraRestPoint = camera.transform.position;
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        
+        //Move character and the camera focus to the desired spawn
+        //position on each scene. Hardcoded garbage. Also has a conflict
+        //where the camera z position should be the defaultZ but the code
+        //passes a hardcoded number 
+        // TODO: find a better, nonhardcoded solution
+
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log("The scene index is " + currentSceneIndex);
+        Vector3 spawnpoint;
+
+        switch (currentSceneIndex)
+        {
+            case 1:
+                //Intro + Cabin
+                Debug.Log("entered Forest Cabin");
+                break;
+            case 2:
+                //Forest 1-1
+                spawnpoint = new Vector3(-20, 6, 0);
+                gameObject.transform.position = spawnpoint;
+                reinitializeCameraObject(spawnpoint);
+                Debug.Log("entered Forest 1-1");
+                break;
+            case 3:
+                //Forest 1-2
+                spawnpoint = new Vector3(0, 6, 0);
+                gameObject.transform.position = spawnpoint;
+                reinitializeCameraObject(spawnpoint);
+                Debug.Log("entered Forest 1-2");
+                break;
+            case 4:
+                //Forest 1-3
+                spawnpoint = new Vector3(0, 6, 0);
+                gameObject.transform.position = spawnpoint;
+                reinitializeCameraObject(spawnpoint);
+                Debug.Log("entered Forest 1-3");
+                break;
+        }
 
     }
 
+    void Start()
+    {
+        //Initializing the camera object and the values
+        camera = Camera.main;
+        defaultZ = camera.transform.position.z;
+        marginBottomFloor = camera.transform.position.y;
+        cameraRestPoint = camera.transform.position;
+        DontDestroyOnLoad(this);
+
+    }
+
+    
     // Update is called once per frame
     void Update()
     {
@@ -75,18 +133,21 @@ public class CameraFocus : MonoBehaviour
                defaultZ);
             camera.transform.position = cameraRestPoint;
         }
-        if (newY < marginDown)
+        if (newY < marginDown || groundCheck)
         {
             cameraRestPoint = new Vector3(
                cameraRestPoint.x,
-               (cameraRestPoint.y + (newY - marginDown)),
+               ((cameraRestPoint.y + (newY - marginDown)) + 2.5f),
                defaultZ);
+        //    if (cameraRestPoint.y > marginBottomFloor)
+        //    {
             camera.transform.position = cameraRestPoint;
+        //    }
         }
 
         // camera.transform.position = new Vector3(newX, newY, defaultZ);
         UpdateMargins();
-        OnDrawGizmos();
+        //OnDrawGizmos();
 
     }
 
@@ -97,6 +158,13 @@ public class CameraFocus : MonoBehaviour
         edgeVectorBottomLeft = Camera.main.ViewportToWorldPoint(ViewportBottomLeft);
         edgeVectorTopRight = Camera.main.ViewportToWorldPoint(ViewportTopRight);
         edgeVectorBottomRight = Camera.main.ViewportToWorldPoint(ViewportBottomRight);
+    }
+
+    void reinitializeCameraObject(Vector3 newfocus)
+    {
+        camera = Camera.main;
+        camera.transform.position = newfocus;
+
     }
 
     public void OnDrawGizmos()
